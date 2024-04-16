bf1edd8a8808ca6f0b5675bf2f5b74928c9c114d
JoseJavierGJ <jjosejavier@hotmail.com>
2024-04-16 09:35:36 -0600

    Conexion con login

---
+++ pages/dashboard/usuarios/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    AQUI VA LA TABLA
+  </v-row>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  layout: 'dashboard',
+  auth: true,
+  computed: {
+    ...mapState({
+      token: state => state.token
+    })
+  },
+  mounted () {
+    this.obtenerUsuarios()
+  },
+  methods: {
+    obtenerUsuarios () {
+      const url = process.nextTick.URL_BASE + '/get-all-users'
+      const config = {
+        headers: {
+          Authorization: `Bearer ${this.token}`
+        }
+      }
+      this.$axios.get(url, config)
+        .then((res) => {
+          console.log('response => ', res)
+        })
+        .catch((error) => {
+          console.log('error => ', error)
+        })
+    }
+  }
+}
+</script>
