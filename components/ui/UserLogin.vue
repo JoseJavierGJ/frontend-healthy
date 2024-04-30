2f255d80147d0457fcc0b015efc91156b8298a55
JoseJavierGJ <jjosejavier@hotmail.com>
2024-04-30 09:18:58 -0600

    Actualizar y agregar usuarios

--- components/ui/UserLogin.vue
+++ components/ui/UserLogin.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-card rounded color="#EC9143" class="pa-7">
+  <v-card rounded color="#EC9143" class="pa-7" style="height: 70%!important; margin-right: 40px;">
     <v-card-title>
       <v-row class="rowCard fontTitle">
         Welcome
       </v-row>
-      <v-row class="rowCard fontTitle">
+      <v-row class="rowCard2 fontTitle">
         Back
       </v-row>
     </v-card-title>
@@ -13,22 +13,117 @@
         <v-text-field v-model="email" rounded label="E-Mail" outlined />
       </v-row>
       <v-row>
-        <v-text-field v-model="password" rounded label="E-password" outlined type="password" />
+        <v-text-field v-model="password" rounded label="Password" outlined type="password" />
       </v-row>
     </v-card-text>
     <v-card-actions>
       <v-col cols="12">
-        <v-row class="rowCard">
+        <v-row>
           <v-btn block class="pa-2" color="#134700" @click="login">
             <span style="text-transform: none; color: white;">Log-in</span>
           </v-btn>
         </v-row>
-        <v-row class="rowCard">
-          <span> If you didn't have account</span>
-          <a href="">Sign-Up</a>
+        <v-row>
+          <v-spacer />
+          <span class="rowCard3">
+            If You Don't Have Account
+            &nbsp; <!-- Espacio en blanco -->
+            <a @click="showDialog = true">Sign Up</a>
+          </span>
         </v-row>
       </v-col>
     </v-card-actions>
+    <v-dialog
+      v-model="showDialog"
+      persistent
+      width="500"
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-card-title>Agregar Usuario</v-card-title>
+        <v-card-text>
+          <v-row width="100%">
+            <v-text-field
+              v-model="emailNuevo"
+              class="pa-2 ma-2"
+              label="Correo"
+              placeholder="Escribe el correo"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="passwordNuevo"
+              class="pa-2 ma-2"
+              label="Password"
+              placeholder="Escribe la contraseña"
+              outlined
+              type="password"
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="nombre"
+              class="pa-2 ma-2"
+              label="Nombre"
+              placeholder="Escribe el nombre"
+              outlined
+            />
+          </v-row><v-row width="100%">
+            <v-text-field
+              v-model="apaterno"
+              class="pa-2 ma-2"
+              label="Apellido paterno"
+              placeholder="Escribe el apellido paterno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="amaterno"
+              class="pa-2 ma-2"
+              label="Apellido materno"
+              placeholder="Escribe el apellido materno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="direccion"
+              class="pa-2 ma-2"
+              label="Dirección"
+              placeholder="Escribe la direccion"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="telefono"
+              class="pa-2 ma-2"
+              label="Telefono"
+              placeholder="Escribe el telefono"
+              outlined
+            />
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-col cols="6">
+            <v-btn block color="green" @click="registrarUsuario">
+              <span style="text-transform: none; color: white;">
+                Registrar
+              </span>
+            </v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn block color="red" @click="showDialog = false">
+              <span style="text-transform: none; color: white;">
+                Cancelar
+              </span>
+            </v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -37,11 +132,20 @@ export default {
   data () {
     return {
       email: null,
-      password: null
+      password: null,
+      showDialog: null,
+      emailNuevo: null,
+      passwordNuevo: null,
+      nombre: null,
+      apaterno: null,
+      amaterno: null,
+      direccion: null,
+      telefono: null
     }
   },
   methods: {
     async login () {
+      // eslint-disable-next-line no-console
       await console.log('@@@ datos => ', this.email, this.password)
       const sendData = {
         email: this.email,
@@ -50,24 +154,62 @@ export default {
       await this.$auth.loginWith('local', {
         data: sendData
       }).then(async (res) => {
+        // eslint-disable-next-line no-console
         const result = await res.data
         if (result.message === 'success') {
           this.$store.commit('setToken', result.token)
           this.$router.push('/dashboard')
         }
       }).catch((err) => {
+        // eslint-disable-next-line no-console
         console.log('@@@ error => ', err)
       })
+    },
+    registrarUsuario () {
+      const url = '/register'
+      const data = {
+        email: this.emailNuevo,
+        password: this.passwordNuevo,
+        nombre: this.nombre,
+        apaterno: this.apaterno,
+        amaterno: this.amaterno,
+        direccion: this.direccion,
+        telefono: this.telefono
+      }
+      this.$axios.post(url, data)
+        .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log('@@ res => ', res)
+          if (res.data.message === 'User registered successfully') {
+            this.showDialog = false
+          }
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log('@@ error => ', error)
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-.rowCard {
+.rowCard{
   width: 100%;
   display: flex;
   justify-content: center;
+}
+.rowCard2{
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  padding-bottom: 20px;
+}
+.rowCard3{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
 }
 .fontTitle {
   font-size: 52px!important;
--- pages/dashboard/usuarios/index.vue
+++ pages/dashboard/usuarios/index.vue
@@ -25,7 +25,7 @@
               </v-btn>
             </v-col>
             <v-col cols="6">
-              <v-btn icon color="warning" @click="UpdateUser(item)">
+              <v-btn icon color="warning" @click="updateUser(item)">
                 <v-icon>mdi-account</v-icon>
               </v-btn>
             </v-col>
@@ -42,6 +42,50 @@
       <v-card>
         <v-card-title>Agregar Usuarios</v-card-title>
         <v-card-text>
+          <v-row width="100%">
+            <v-text-field
+              v-model="nombre"
+              class="pa-2 ma-2"
+              label="Nombre"
+              placeholder="Escribe el nombre"
+              outlined
+            />
+          </v-row><v-row width="100%">
+            <v-text-field
+              v-model="apaterno"
+              class="pa-2 ma-2"
+              label="Apellido paterno"
+              placeholder="Escribe el apellido paterno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="amaterno"
+              class="pa-2 ma-2"
+              label="Apellido materno"
+              placeholder="Escribe el apellido materno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="direccion"
+              class="pa-2 ma-2"
+              label="Dirección"
+              placeholder="Escribe la direccion"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="telefono"
+              class="pa-2 ma-2"
+              label="Telefono"
+              placeholder="Escribe el telefono"
+              outlined
+            />
+          </v-row>
           <v-row width="100%">
             <v-text-field
               v-model="email"
@@ -87,13 +131,101 @@
         <v-card-actions>
           <v-col cols="6">
             <v-btn block color="green" @click="borrarUsuario">
+              <span style="text-transform: none; color: white;">Borrar</span>
+            </v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn block color="red" @click="showDelete=false">
+              <span style="text-transform: none; color: white;">Cancelar</span>
+            </v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="showUpdate"
+      persistent
+      width="500"
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-card-title>Actualizar Usuario</v-card-title>
+        <v-card-text>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.nombre"
+              class="pa-2 ma-2"
+              label="Nombre"
+              placeholder="Escribe el nombre"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.apaterno"
+              class="pa-2 ma-2"
+              label="Apellido paterno"
+              placeholder="Escribe el apellido paterno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.amaterno"
+              class="pa-2 ma-2"
+              label="Apellido materno"
+              placeholder="Escribe el apellido materno"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.direccion"
+              class="pa-2 ma-2"
+              label="Dirección"
+              placeholder="Escribe la direccion"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.telefono"
+              class="pa-2 ma-2"
+              label="Telefono"
+              placeholder="Escribe el telefono"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.email"
+              class="pa-2 ma-2"
+              label="Correo"
+              placeholder="Escribre tu Password"
+              outlined
+            />
+          </v-row>
+          <v-row width="100%">
+            <v-text-field
+              v-model="userToUpdate.password"
+              class="pa-2 ma-2"
+              label="Password"
+              placeholder="Escribre tu Password"
+              outlined
+              type="password"
+            />
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-col cols="6">
+            <v-btn block color="green" @click="actualizarUsuario">
               <span style="text-transform: none; color: white;">
-                Borrar
+                Actualizar
               </span>
             </v-btn>
           </v-col>
           <v-col cols="6">
-            <v-btn block color="red" @click="showDialog = false">
+            <v-btn block color="red" @click="showUpdate = false">
               <span style="text-transform: none; color: white;">
                 Cancelar
               </span>
@@ -115,14 +247,44 @@ export default {
       usuarios: [],
       headers: [
         {
+          text: 'Nombre',
+          value: 'nombre',
+          align: 'center',
+          sortable: true
+        },
+        {
+          text: 'Apellido paterno',
+          value: 'apaterno',
+          align: 'center',
+          sortable: true
+        },
+        {
+          text: 'Apellido materno',
+          value: 'amaterno',
+          align: 'center',
+          sortable: true
+        },
+        {
           text: 'Email',
           value: 'email',
           align: 'center',
           sortable: true
         },
+        // {
+        //   text: 'Password',
+        //   value: 'password',
+        //   align: 'center',
+        //   sortable: true
+        // },
         {
-          text: 'Password',
-          value: 'password',
+          text: 'Dirección',
+          value: 'direccion',
+          align: 'center',
+          sortable: true
+        },
+        {
+          text: 'Telefono',
+          value: 'telefono',
           align: 'center',
           sortable: true
         },
@@ -136,7 +298,14 @@ export default {
       email: null,
       password: null,
       userToDelete: null,
-      showDelete: false
+      showDelete: false,
+      nombre: null,
+      apaterno: null,
+      amaterno: null,
+      direccion: null,
+      telefono: null,
+      userToUpdate: {},
+      showUpdate: false
     }
   },
   computed: {
@@ -152,12 +321,14 @@ export default {
       const url = '/get-all-users'
       this.$axios.get(url)
         .then((res) => {
+          // eslint-disable-next-line no-console
           console.log('response => ', res)
           if (res.data.message === 'success') {
             this.usuarios = res.data.users
           }
         })
         .catch((error) => {
+          // eslint-disable-next-line no-console
           console.log('error => ', error)
         })
     },
@@ -165,37 +336,72 @@ export default {
       const url = '/register'
       const data = {
         email: this.email,
-        password: this.password
+        password: this.password,
+        nombre: this.nombre,
+        apaterno: this.apaterno,
+        amaterno: this.amaterno,
+        direccion: this.direccion,
+        telefono: this.telefono
       }
       this.$axios.post(url, data)
         .then((res) => {
-          console.log('@@ res =>', res)
+          // eslint-disable-next-line no-console
+          console.log('@@ res => ', res)
           if (res.data.message === 'User registered successfully') {
             this.showDialog = false
+            this.obtenerUsuarios()
           }
-          this.obtenerUsuarios()
         })
         .catch((error) => {
+          // eslint-disable-next-line no-console
           console.log('@@ error => ', error)
         })
     },
     borrarUsuario () {
       const url = `/users/${this.userToDelete.email}`
+      // eslint-disable-next-line no-console
+      console.log('@@@ url => ', url)
       this.$axios.delete(url)
         .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log('@@ res => , res')
           if (res.status === 204) {
             this.showDelete = false
             this.obtenerUsuarios()
           }
         })
         .catch((err) => {
+          // eslint-disable-next-line no-console
           console.log('@@ err => ', err)
         })
     },
     deleteUser (user) {
       this.userToDelete = user
       this.showDelete = true
+      // eslint-disable-next-line no-console
       console.log('@@@ user => ', this.userToDelete)
+    },
+    updateUser (user) {
+      this.userToUpdate = user
+      this.showUpdate = true
+    },
+    actualizarUsuario () {
+      const url = `/users/${this.userToUpdate.email}`
+      // eslint-disable-next-line no-console
+      console.log('@@@ url => ', url)
+      this.$axios.put(url, this.userToUpdate)
+        .then((res) => {
+          // eslint-disable-next-line no-console
+          console.log('@@@ res => , res')
+          if (res.data.message === 'success') {
+            this.showUpdate = false
+            this.obtenerUsuarios()
+          }
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.log('@@ err => ', err)
+        })
     }
   }
 }
